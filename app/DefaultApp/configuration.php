7f358ab9113929abe6d16fc0810609d4742e58ca
fater04 <wilkerdorvelus@yahoo.com>
2020-02-20 11:20:09 -0500

    modification online

--- app/DefaultApp/configuration.php
+++ app/DefaultApp/configuration.php
@@ -1,9 +1,9 @@
 <?php
 $database=array(
     "serveur"=>"localhost",
-    "nom_base"=>"event",
-    "utilisateur"=>"root",
-    "motdepasse"=>""
+    "nom_base"=>"biostmwu_Event",
+    "utilisateur"=>"biostmwu_bios",
+    "motdepasse"=>"Haiti2019"
 );
 
 $from=array(
@@ -23,6 +23,26 @@ $configuration=array(
     "url"=>$_GET['url'],
     "database"=>$database,
     "configurationEmail"=>$configurationEmail,
-    "dossierProjet"=>"/event",
+    "dossierProjet"=>"",
     "nomApp"=>"DefaultApp"
 );
+
+//------------EXEMPLE ENVOYER EMAIL-------------
+//une seule adresse email
+/*$a="alcindorlos@gmail.com,los";
+
+//plusieur adresse email
+
+$a=array(
+   "alcindorlos@gmail.com,los" ,
+    "serveurlos@gmail.com,serveurlos"
+);
+$contenue="<h1>Une contenue en html</h1>";
+$m=\systeme\Model\Model::envoyerEmail($a,"test plusier email",$contenue);
+if($m=="ok"){
+   echo "message envoyer avec success";
+}else{
+ echo $m;
+}*/
+
+
