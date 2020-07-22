38e7edbaefb1624315cbc81faa6d22a8e3babda0
fater04 <wilkerdorvelus@yahoo.com>
2020-07-21 21:54:45 -0400

    fix bug

--- app/DefaultApp/Controlleurs/ApiControlleur.php
+++ app/DefaultApp/Controlleurs/ApiControlleur.php
@@ -100,9 +100,9 @@ class ApiControlleur extends Controlleur
         header("Content-Type: Application/json; charset=UTF-8");
         if (isset($_GET['token']) && isset($_GET['device']) && isset($_GET['phone']) && isset($_GET['message'])) {
             $token = Utilisateur::Rechercher($_GET['token']);
-            if ($token == "0") {
+            if ($token != "0") {
                 $device = Setting::rechercher($_GET['device']);
-                if($device!=0){
+                if($device!="0"){
                     $phone = $_GET['phone'];
                     $message = $_GET['message'];
                     $result = Participant::ajouter($phone, $message, $device->getDeviceId(), $token->getId());
